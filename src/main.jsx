c2cb90930ac0830f48136de179a96007bcc3e151
mattstyles <matt@veryfizzyjelly.com>
2015-10-12 21:01:24 +0100

    #add css utils

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import dispatcher from './dispatchers/appDispatcher'
+import logger from 'utils/logger'
+import dispatcher from 'dispatchers/appDispatcher'
 
 
 function render() {
-    ReactDOM.render( <App />, document.querySelector( '.js-main' ) )
+    ReactDOM.render( <App />, document.querySelector( '.js-app' ) )
 }
 
 
@@ -18,11 +19,12 @@ class App extends React.Component {
 
     render() {
         return (
-            <div className="container">
+            <div className="container u-fit">
                 <h1>Odyssey Nova</h1>
             </div>
         )
     }
 }
 
+logger.info( 'Bootstrapping...' )
 render()
