ebf6034c49e3db2c9efe7f9cfb0e17f381093ec5
Matt Styles <matt@veryfizzyjelly.com>
2015-10-19 09:05:04 +0100

    #add thrust components to ship entity by default

--- src/components/entities/ship.js
+++ src/components/entities/ship.js
@@ -46,5 +46,36 @@ export default class Ship extends PhysicalEntity {
         return this
     }
 
+    /*-----------------------------------------------------------*
+     *
+     *  Add thrust behaviours
+     *
+     *-----------------------------------------------------------*/
+
+    applyMainThruster = () => {
+        this.linearThrust.forEach( thruster => {
+            this.body.applyForceLocal( thruster.magnitude, thruster.offset )
+        })
+    }
+
+    applyTurnLeft = () => {
+        this.body.angularVelocity = -this.turnThrust
+    }
+
+    applyTurnRight = () => {
+        this.body.angularVelocity = this.turnThrust
+    }
+
+    // Banking is almost like strafing, but results in a slight opposite turn as well
+    // The slight offset implies the banking thrusters are located behind the
+    // center of gravity, which accounts for the slight turn imparted
+    applyBankLeft = () => {
+        this.body.applyForceLocal( [ this.bankThrust, 0 ], [ 0, -1 ] )
+    }
+    
+    applyBankRight = () => {
+        this.body.applyForceLocal( [ -this.bankThrust, 0 ], [ 0, -1 ] )
+    }
+
 
 }
