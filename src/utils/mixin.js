843cb35b9a1a2fe2715f1684796fba14770873bb
mattstyles <matt@veryfizzyjelly.com>
2015-10-19 21:06:40 +0100

    #add mixin composer

---
+++ src/utils/mixin.js
@@ -0,0 +1,20 @@
+/**
+ * Based on [this gist](https://gist.github.com/sebmarkbage/fac0830dbb13ccbff596)
+ * by [Sebastian Markbåge](https://github.com/sebmarkbage)
+ */
+
+import noop from 'utils/noop'
+
+export default function mixin( ...components ) {
+    // Add methods here to avoid throwing errors
+    var base = class {
+        update() {}
+        render() {}
+    }
+
+    components.forEach( component => {
+        base = component( base )
+    })
+
+    return base
+}
